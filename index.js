b2c17ac2f91cae3019903049f6202adfb549d1f2
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-26 18:54:50 -0400

    set up app and configure server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+// db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
